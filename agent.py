015bbbc1531f78be6557de770f69a084af06f90d
koushal <koushal@koushal-Inspiron-5593>
2025-03-26 00:09:11 +0530

    Added agent.py

---
+++ agent.py
@@ -0,0 +1,10 @@
+This is your Python function:
+
+```python
+def print_message():
+    print("Hello world, Laxman")
+
+# Calling the function
+print_message()
+```
+This function `print_message()` prints a string `Hello world, Laxman` when it is called. To call the function, just use the function name followed by parenthesis as shown above.
